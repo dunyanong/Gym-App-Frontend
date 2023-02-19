0f98e95aa01d3cb42642e5ed6c91a9364a4c29ba
Dun Yan <97597966+dunyanong@users.noreply.github.com>
2023-02-19 16:17:08 +0800

    Update useLogin.js

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,36 +1,33 @@
-import { useState } from "react"
-import { useAuthContext} from './useAuthContext'
+import { useState } from "react";
+import { useAuthContext } from './useAuthContext';
 
 export const useLogin = () => {
     const [error, setError] = useState(null);
     const [isLoading, setIsLoading] = useState(null);
-    const { dispatch } = useAuthContext()
-  
+    const { dispatch } = useAuthContext();
+
     const login = async (email, password) => {
-        setIsLoading(true)
-        setError(null)
+        setIsLoading(true);
+        setError(null);
 
         const response = await fetch('/api/user/login', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({email, password})
-        })
-        const json = await response.json()
+        });
 
-        if (!response.ok)  {
-            setIsLoading(false)
-            setError(json.error)
-        }
-        if (response.ok)  {
+        if (response.ok) {
+            const json = await response.json();
             // save the users to local storage
             localStorage.setItem('user', JSON.stringify(json));
-
             // update the auth context
-            dispatch({type: 'LOGIN', payload: json})
-
-            setIsLoading(false);
+            dispatch({type: 'LOGIN', payload: json});
+        } else {
+            const json = await response.json();
+            setError(json.error);
         }
+        setIsLoading(false);
     }
 
-    return { login, isLoading, error}
-}
+    return { login, isLoading, error }
+};
